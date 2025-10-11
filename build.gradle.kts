fe26acdfec4defba6ab7bd42dc9f8c42d2539aef
TomasWild <tomaswild@outlook.com>
2025-10-11 08:30:51 -0300

    chore: init

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+}
+
+group = "com.wild"
+version = "0.0.1-SNAPSHOT"
+description = "ecommerce"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.bundles.spring.boot.starter)
+    implementation(libs.jspecify)
+    implementation(libs.dotenv.java)
+    implementation(libs.springdoc.openapi)
+    runtimeOnly(libs.mysql)
+    runtimeOnly(libs.h2)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    testImplementation(libs.bundles.spring.boot.test)
+    testImplementation(libs.bundles.testcontainers)
+    testImplementation(libs.mockito.core)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/wild/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.wild.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+}
